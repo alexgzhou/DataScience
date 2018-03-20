c7fae6430d83e281ebd0e3eb1ada6696d9df060f
Woo <wck.bme@qq.com>
2018-03-20 13:47:22 +0800

    change improve function

--- LabelCorrection-KingsWoo/source/evaluate.py
+++ LabelCorrection-KingsWoo/source/evaluate.py
@@ -76,8 +76,11 @@ class Evaluate:
 
         values_ori = self.evaluator(y, y_ori)
         values_adv = self.evaluator(y, y_adv)
+        
         values_diff = [signs[i] * (values_adv[names[i]] - values_ori[names[i]]) for i in range(6)]
-        return sum([- math.exp(-k * values_diff[i]) + 1 for i in range(6)])
+        values_base = [2 * math.sqrt(values_ori[names[i]] * (1 - values_ori[names[i]])) for i in range(6)]
+
+        return sum([- math.exp(-k * values_diff[i]/values_base[i]) + 1 for i in range(6)])
 
 
 
